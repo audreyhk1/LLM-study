cf2399cf08a30ed49771e638fe2242b3055d5a16
audreyhk1 <120613322+audreyhk1@users.noreply.github.com>
2024-10-30 12:43:39 +0000

    debugging fix...code should work, just one update error

--- probability_csv.py
+++ probability_csv.py
@@ -22,11 +22,10 @@ def main():
     cols = data_df.columns
     for i in range(2, len(cols), 2):
         llm_probability = find_probabilities_per_llm(llm_name=cols[i], llm_ans=data_df[cols[i]], llm_concordant=data_df[cols[i + 1]], large_file=data_df)
-        print(llm_probability)
-        # probability_csv = pd.concat([probability_csv, llm_probability], ignore_index=False)
- 
+        probability_csv = pd.concat([probability_csv, llm_probability], ignore_index=False)
+    
     # write df to csv
-    # probability_csv.to_csv("probability.csv")
+    probability_csv.to_csv("analysis/probability.csv")
 
 
 """
@@ -37,7 +36,7 @@ def main():
 """
 def find_probabilities_per_llm(llm_name, llm_ans, llm_concordant, large_file):
     # create dataframe to store all probabilities for the llm
-    llm_probabilities = pd.DataFrame(0, index=["Correct", "Total"], columns=range(11))
+    llm_probabilities = pd.DataFrame(0, index=["Correct", "Total", llm_name], columns=range(11))
     
     # loop through every question
     for i in range(NQUESTIONS):
@@ -46,8 +45,10 @@ def find_probabilities_per_llm(llm_name, llm_ans, llm_concordant, large_file):
         # check if the answer is correct
         if is_answer_correct(question_num=0, ans=llm_ans.iloc[i], large_df=large_file):
             llm_probabilities.iat[0, ind] += 1
-        llm_probabilities[1, ind] += 1
-    return llm_probabilities[0].div(llm_probabilities[1], axis=0)
+        llm_probabilities.iat[1, ind] += 1
+
+    llm_probabilities.loc[llm_name] = llm_probabilities.loc["Correct"].div(llm_probabilities.loc["Total"], axis=0).fillna(0).astype('float64')
+    return llm_probabilities.loc[llm_name]
 
 def is_answer_correct(question_num, ans, large_df, col = "Correct Answer"):
     if ans in large_df[col].iloc[question_num]:
